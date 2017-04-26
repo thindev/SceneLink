5b551b4c317e7bbda5ca5922577a37aee5d80fd7
thindev <838876225@qq.com>
2017-04-27 00:44:56 +0800

    完成bottomnavigationbar外观设计。

--- app/src/main/java/com/calenstudio/scenelink/MainActivity.java
+++ app/src/main/java/com/calenstudio/scenelink/MainActivity.java
@@ -1,45 +1,63 @@
 package com.calenstudio.scenelink;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+
+
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+
+import android.support.v7.app.AppCompatDelegate;
+
+import android.support.v7.widget.Toolbar;
+
+import android.view.Menu;
 import android.widget.TextView;
 
+
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+
+
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 public class MainActivity extends AppCompatActivity {
-
+static {
+    AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+}
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initActionBar();
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        this.initBottomNavigationBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu_main, menu);//加载menu文件到布局
+
+        return true;
+    }
+
+
+    private  void initActionBar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
+    }
+
+    private void initBottomNavigationBar()
+    {
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.bnv_link, R.string.title_link))
+                .addItem(new BottomNavigationItem(R.drawable.bnv_find, R.string.title_find))
+                .addItem(new BottomNavigationItem(R.drawable.bnv_resource, R.string.title_resource))
+                .addItem(new BottomNavigationItem(R.drawable.bnv_my, R.string.title_my))
+                .initialise();
+
     }
 
 }
