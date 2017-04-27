911cf1e04863881b8ff766dda9ed6445a8c779d4
thindev <838876225@qq.com>
2017-04-27 16:57:38 +0800

    添回底部导航对应的Fragments

--- app/src/main/java/com/calenstudio/scenelink/MainActivity.java
+++ app/src/main/java/com/calenstudio/scenelink/MainActivity.java
@@ -1,8 +1,12 @@
 package com.calenstudio.scenelink;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.app.AppCompatDelegate;
@@ -13,51 +17,129 @@ import android.view.Menu;
 import android.widget.TextView;
 
 
-
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 
 
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.calenstudio.scenelink.view.mainpage.FindFragment;
+import com.calenstudio.scenelink.view.mainpage.LinkFragment;
+import com.calenstudio.scenelink.view.mainpage.MyFragment;
+import com.calenstudio.scenelink.view.mainpage.ResourcesFragment;
 
-public class MainActivity extends AppCompatActivity {
-static {
-    AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-}
-    private TextView mTextMessage;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity
+        implements FindFragment.OnFragmentInteractionListener
+        ,LinkFragment.OnFragmentInteractionListener,
+        MyFragment.OnFragmentInteractionListener,
+        ResourcesFragment.OnFragmentInteractionListener
+
+{
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+    private Toolbar mActionBar;
+    private ArrayList<Fragment> mFragments;
+    private BottomNavigationBar mBottomNavigationBar;
+    private int mLastSelectPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initActionBar();
-        mTextMessage = (TextView) findViewById(R.id.message);
         this.initBottomNavigationBar();
+        setActionBarTitle(0);
+        selectContentView(0);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.option_menu_main, menu);//加载menu文件到布局
-
         return true;
     }
 
 
-    private  void initActionBar(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle(R.string.app_name);
+    private void initActionBar() {
+        mActionBar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(mActionBar);
+
     }
 
-    private void initBottomNavigationBar()
+    private  void  initFragments()
     {
-        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
-        bottomNavigationBar
+        mFragments=new ArrayList<>();
+        mFragments.add(LinkFragment.newInstance("",""));
+        mFragments.add(FindFragment.newInstance("",""));
+        mFragments.add(ResourcesFragment.newInstance("",""));
+        mFragments.add(ResourcesFragment.newInstance("",""));
+    }
+    private void initBottomNavigationBar() {
+        if(mFragments==null){
+            this.initFragments();
+        }
+        mBottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        mBottomNavigationBar
                 .addItem(new BottomNavigationItem(R.drawable.bnv_link, R.string.title_link))
                 .addItem(new BottomNavigationItem(R.drawable.bnv_find, R.string.title_find))
                 .addItem(new BottomNavigationItem(R.drawable.bnv_resource, R.string.title_resource))
                 .addItem(new BottomNavigationItem(R.drawable.bnv_my, R.string.title_my))
                 .initialise();
+        mBottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                setActionBarTitle(position);
+                selectContentView(position);
+                mLastSelectPosition=position;
+            }
 
+            @Override
+            public void onTabUnselected(int position) {
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+                setActionBarTitle(position);
+            }
+        });
     }
+    private void setActionBarTitle(int position) {
+        switch (position) {
+            case 0:
+                mActionBar.setTitle(R.string.title_link);
+                break;
+            case 1:
+                mActionBar.setTitle(R.string.title_find);
+                break;
+            case 2:
+                mActionBar.setTitle(R.string.title_resource);
+                break;
+            case 3:
+                mActionBar.setTitle(R.string.title_my);
+                break;
+            default:
+                mActionBar.setTitle(R.string.app_name);
+        }
+    }
+    private  void  selectContentView(int position) {
+        if (position >= 0 && mFragments != null && mFragments.size() > position) {
+            FragmentTransaction ft= this.getSupportFragmentManager()
+                    .beginTransaction();
+            if(mLastSelectPosition>position)
+            {
+                ft.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+            else
+            {
+                 ft.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+
+
+                    ft.replace(R.id.content,mFragments.get(position))
+                    .commit();
+
+        }
+    }
+
 
 }
